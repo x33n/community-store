e6b304089b1b197f098564da4f5437096f8bc875
modularview <git@modularview.com>
2015-03-24 13:18:10 +0200

    add flip animation to login page

--- views/login/controller.js
+++ views/login/controller.js
@@ -5,25 +5,41 @@
  */
 angular.module('LoginView', [])
 
+
+/*
+ * @ngdoc directive
+ * @name flipLoginPanel
+ * @module CoomunityStore.LoginView
+ */
+.directive('flipLoginPanel', function(){
+    return {
+        restrict: 'E',
+        template: '<a ng-click="flipPanels($event)" ng-transclude></a>',
+        replace: true,
+        transclude: true
+    };
+})
+
+
 .controller('LoginViewController', function($scope, $state, $mdBottomSheet){
     
     $scope.pageClass = 'page-login';
+    
     $scope.stateChange = 'loginMode';
+    $scope.flipPanels = function(event){
+        var buttonEl = angular.element(event.target);
+        var parent = buttonEl.parents('form');
+        
+        parent.toggleClass('animated');
+        $scope.stateChange = ($scope.stateChange === 'loginMode') ? 'registerMode' : 'loginMode';
+        event.preventDefault();
+    };
     
     $scope.showBottomBar = function(event){
         $mdBottomSheet.show({
             templateUrl: 'views/login/bottom-bar.html',
             targetEvent: event
         });
-    };
-    
-    $scope.changeFormState = function(event){
-        if(event.target.id === 'loginFormTrigger'){
-            $scope.stateChange = 'registerMode';
-        }
-        else{
-            $scope.stateChange = 'loginMode';
-        }
     };
     
     $scope.doLogin = function(form){
